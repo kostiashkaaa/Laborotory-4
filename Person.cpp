d2d6dd55e80eb20520c229822dde7a8d5c1a9b82
Konstanitn <90149772+kostiashkaaa@users.noreply.github.com>
2025-11-29 18:13:19 +0300

    Update Person.cpp

--- Person.cpp
+++ Person.cpp
@@ -1,7 +1,6 @@
 #include "Person.h"
 #include <iostream>
 
-// Конструкторы изменены
 Person::Person() : name(""), phoneNumber("") {}
 
 Person::Person(const std::string& name, const std::string& phoneNumber)
@@ -14,28 +13,17 @@ std::string Person::getName() const {
 }
 
 void Person::setName(const std::string& name) {
-    // --- ИСПРАВЛЕНО ---
-    // БЫЛО: this.name = name;
-    // НАДО:
     this->name = name;
 }
 
-// Реализация методов для phoneNumber
 std::string Person::getPhoneNumber() const {
     return phoneNumber;
 }
 
 void Person::setPhoneNumber(const std::string& phone) {
-    // --- ИСПРАВЛЕНО ---
-    // БЫЛО: this.phoneNumber = phone;
-    // НАДО:
     this->phoneNumber = phone;
-    // (Также можно было написать просто: phoneNumber = phone;
-    // так как имена не конфликтуют, но this-> тоже верно)
 }
 
-// Имя метода изменено
 void Person::display() const {
-    // Я также добавил std::endl для корректного переноса строки
     std::cout << "Имя: " << name << ", Телефон: " << phoneNumber << std::endl;
 }
