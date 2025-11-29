00442c5e79dd43a2b3f5768d1e13f70f82eb13ff
Konstanitn <90149772+kostiashkaaa@users.noreply.github.com>
2025-11-29 18:14:20 +0300

    Update VIPClient.cpp

--- VIPClient.cpp
+++ VIPClient.cpp
@@ -3,16 +3,12 @@
 
 VIPClient::VIPClient() : Client(), discount(0.0), personalManager(""), loyaltyProgram() {}
 
-// --- ИСПРАВЛЕННАЯ СТРОКА ---
-// БЫЛО: loyaltyProgram(discount, 0)
-// СТАЛО: loyaltyProgram("VIP", discount)
-// Мы передаем "VIP" как строку статуса и discount как скидку.
+
 VIPClient::VIPClient(const std::string& name, const std::string& phoneNumber,
                      double balance, double discount, const std::string& manager)
     : Client(name, phoneNumber, balance), discount(discount),
     personalManager(manager), loyaltyProgram("VIP", discount) {}
 
-// ДОБАВЬТЕ:
 double VIPClient::getDiscount() const {
     return discount;
 }
@@ -37,6 +33,5 @@ void VIPClient::display() const {
     Client::display();
     std::cout << "Скидка: " << discount << "%" << std::endl;
     std::cout << "Персональный менеджер: " << personalManager << std::endl;
-    // loyaltyProgram.display(); // У LoyaltyProgram нет метода display()
-    loyaltyProgram.displayLoyaltyInfo(); // У нее есть displayLoyaltyInfo()
+    loyaltyProgram.displayLoyaltyInfo();
 }
